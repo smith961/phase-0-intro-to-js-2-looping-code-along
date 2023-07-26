bb4ec26627bcc174378c8d24c1a6680826bf85c5
unknown <smithbabawande@gmail.com>
2023-07-26 03:59:01 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const name = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+
+function writeCards(name , event){
+    let message = [];
+for (let i=0; i < name.length; i++) {
+message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+}
+return message;
+}
+function countDown(){
+    let i=0;
+    while (i < 10){
+        console.log (i)
+        i++;
+    }
+    console.log(i);
+}
+
+
